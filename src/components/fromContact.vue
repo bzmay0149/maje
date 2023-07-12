428f4153166a51766db4358c0180afda70fcd7ec
Shamanesss <mentulbenodorna@gmail.com>
2023-07-12 10:43:53 +0200

    logo

---
+++ src/components/fromContact.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <form action="#" method="GET">
+      <h2 class="form-header">Formulario de contacto</h2>
+      <label for="name">Nombre *</label>
+      <input
+        class="form-input"
+        type="text"
+        id="name"
+        name="name"
+        required
+      />
+
+      <label for="apellido">Apellido *</label>
+      <input
+        class="form-input"
+        type="text"
+        id="apellido"
+        name="apellido"
+        required
+      />
+
+      <label for="email">Email *</label>
+      <input
+        class="form-input"
+        type="email"
+        id="email"
+        name="email"
+        required
+      />
+
+      <label for="telefono">Telefono *</label>
+      <input
+        class="form-input"
+        type="tel"
+        id="telefono"
+        name="telefono"
+        required
+      />
+      <fieldset>
+        <legend>Deseas recibir informacion de las ultimas noticias</legend>
+      <input type="radio" name="informacion" id="si" value="Si">
+        <label for="si">Si </label>
+      
+      
+      
+      <input type="radio" name="informacion" id="no" value="no">
+      
+      <label for="no">No </label>
+      
+    </fieldset>
+      
+      <label for="message">Consulta</label>
+      <textarea
+        class="form-textarea"
+        id="message"
+        name="message"
+      ></textarea>
+      <div>
+        <input type="checkbox" id="accept" name="accept" />
+        <label for="accept">Acepto los términos y condiciones.</label>
+      </div>
+      <button type="submit" class="form-input button" id="button" disabled>
+        Enviar
+      </button>
+    </form>
+        
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <formContact />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<script>
+<script setup>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+import formContact from '@/components/fromContact.vue'
+
 </script>
