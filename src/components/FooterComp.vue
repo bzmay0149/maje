e8e103b4c70ae49ebd32b96ad8ffbe2b15d47860
Juan-Little <juanbenitodevalle@gmail.com>
2023-07-12 13:25:34 +0200

    new maje logo added, img path corrected, gave style according to figma with css scope, file name changed to FooterComp

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,13 +1,13 @@
 <template>
     <footer>
         <section class="footer--boxup">
-            <img src="src\assets\peñascal.png" alt="Peñascal logo">
-            <p>Desarrollamos programas para mejorar la empleabilidad.</p>
+            <img class="logopenascal" src="../assets/peñascal.png" alt="Peñascal logo">
+            <p class="desarrollamos">Desarrollamos programas para mejorar la empleabilidad.</p>
 
         </section>
         <section class="footer--boxdown">
-            <img src="src\assets\maje-logo.png" alt="MAJE logo">
-            <p>
+            <img class="logomaje" src="../assets/logomajeMayBati.png" alt="MAJE logo">
+            <p class="aviso-legal">
                 Aviso legal | Términos legales
             </p>       
         </section>
@@ -16,6 +16,41 @@
 
     </footer>    
 </template>
+<style scoped>
+section{display: flex;}
+.logopenascal{
+    margin: 2rem;
+}
+.footer--boxup {
+    display: flex;
+    background-color:#5BEC86;
+}
+.desarrollamos{
+    margin: auto;
+} 
+aviso-legal{
+    align-items: center;
+}
+.footer--boxdown {
+    display: flex;
+    flex-direction: column;
+    /* background-color:#D17F1F; */
+    background-color: #16A641;
+}
+
+.logomaje {
+    height: 8rem;
+    width: 8rem;
+    margin-top: 5rem;
+    margin-bottom: 3rem;
+margin-left: auto;
+margin-right: auto;
+
+}
+
+
+
+</style>
 
 
 
