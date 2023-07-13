f6ef666c6834e9f139f2a06a4a6ce0f259977ee3
bzmay0149 <mayjuanbaticono@gmail.com>
2023-07-13 13:00:25 +0200

    hamburguesa navegador

--- src/components/NavComp.vue
+++ src/components/NavComp.vue
@@ -1,50 +1,111 @@
 <template>
-    <nav class="navbar navbar-light pt-0">
-      <div class="container-fluid d-flex flex-column align-items-center">
-        <div class="logo  pt-4">
-          <RouterLink to="/" class="navbar-brand ">
-            <img src="../assets/logo-zurtek.png" alt="logo-zurtek" width="160" >
-          </RouterLink>
-        </div>
-        <div class="list mb-4 m">
-          <ul class="nav justify-content-center ">
-            <li class="nav-item">
-              <RouterLink to="/" class="nav-link pt-0  ">Home</RouterLink>
-            </li>
-            <li class="nav-item ">
-              <RouterLink to="/services" class="nav-link pt-0" >Servicios</RouterLink>
-            </li>
-            <li class="nav-item ">
-              <RouterLink to="/about" class="nav-link pt-0" >Contacto</RouterLink>
-            </li>
-          </ul>
-        </div>
+  <nav class="navbar navbar-light pt-0">
+    <div class="container-fluid d-flex align-items-center">
+      <div class="menu-toggle" @click="toggleMenu">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
       </div>
-    </nav>
-  </template>
-  
-  <script setup>
-  // No se requiere el objeto export en el bloque <script setup>
-  // Las variables y funciones se pueden utilizar directamente en el template
-  </script>
-  
-  <style scoped>
-  .container-fluid {
-    background-color: #16A641;
+      <div class="logo mx-lg-0 mx-auto">
+        <RouterLink to="/" class="navbar-brand">
+          <img src="../assets/logo-zurtek.png" alt="logo-zurtek" width="160">
+        </RouterLink>
+      </div>
+      <ul class="menu" :class="{ active: isMenuOpen }">
+        <li class="nav-item">
+          <RouterLink to="/" class="nav-link pt-0">Home</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink to="/services" class="nav-link pt-0">Servicios</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink to="/about" class="nav-link pt-0">Contacto</RouterLink>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const isMenuOpen = ref(false);
+
+function toggleMenu() {
+  isMenuOpen.value = !isMenuOpen.value;
+}
+</script>
+
+<style scoped>
+.container-fluid {
+  background-color: rgb(191, 237, 232);
+}
+
+.logo {
+  text-align: center;
+}
+
+.list {
+  text-align: center;
+}
+
+.nav-link {
+  color: #795225;
+  font-size: x-large;
+  font-weight: bold;
+}
+
+.nav-link:hover {
+  color: #D17F1F;
+}
+
+.menu-toggle {
+  display: none;
+  cursor: pointer;
+}
+
+.menu {
+  display: flex;
+  justify-content: center;
+}
+
+.menu li {
+  list-style: none;
+  margin: 0 10px;
+}
+
+.menu li a {
+  text-decoration: none;
+  color: #795225;
+  font-size: x-large;
+  font-weight: bold;
+}
+
+@media (max-width: 768px) {
+  .menu {
+    display: none;
   }
-  
-  
-   
-  .list {
-    text-align: center; /* Centra horizontalmente los elementos de la lista */
-   
+
+  .menu-toggle {
+    display: block;
   }
-  .nav-link{
-    color: #795225;
-    font-size:x-large;
-    font-weight: bold;
+
+  .menu-toggle .bar {
+    width: 25px;
+    height: 3px;
+    background-color: #795225;
+    margin: 5px 0;
   }
-  .nav-link:hover{
-    color: #D17F1F;
+
+  .menu.active {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
-  </style>
+
+  .logo {
+    text-align: center;
+    margin-left: 0;
+  }
+}
+</style>
