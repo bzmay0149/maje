d241a84a3da37f50e452b711255838b0efeadfbd
Anismelow <anyellmendozalopez@gmail.com>
2023-07-13 09:08:03 +0200

    .

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="home">
     
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
   <FooterComp/>
 </template>
 
 <script setup>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import FooterComp from '@/components/FooterComp.vue'
 
 
