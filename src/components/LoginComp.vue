80d864d38ca131236e8bdf7ac5486260c4b7280c
Anismelow <anyellmendozalopez@gmail.com>
2023-07-18 12:50:43 +0200

    .

--- src/components/LoginComp.vue
+++ src/components/LoginComp.vue
@@ -1,36 +1,45 @@
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+import cors from "cors";
+
+cors();
+
+cors(axios);
+
+// Variables reactivas para almacenar los datos de los campos de entrada
+const username = ref("");
+const password = ref("");
+
+const handleSubmit = async () => {
+  try {
+    await axios.post("http://127.0.0.1:5000/login", {
+      username: username.value,
+      password: password.value,
+    });
+
+    console.log("user", username.value);
+    console.log("password", password.value);
+  } catch (error) {
+    console.log(error);
+  }
+};
+</script>
 <template>
-  <div class="w-50  mx-auto mb-5">
+  <div class="w-50 mx-auto mb-5">
     <h3 class="mt-5">Login Administrador</h3>
-    <form class="login form mt-5 p-5  rounded shadow d-flex flex-column align-items-center" action="">
 
-      <div class="nombre col-sm-12 col-md-6">
-        <label class="form-label" for="username">Nombre de usuario</label>
-        <input
-          class="form-control w-100 mx-auto"
-          type="text"
-          name="username"
-          id=""
-        />
-      </div>
-
-      <div class="contraseña mt-5 col-sm-12 col-md-6 ">
-        <label class="form-label" for="password">Contraseña</label>
-        <input class="form-control w-100 mx-auto" type="text" name="password" id="" />
-      </div>
-      <input
-        class="btn btn-primary mt-5"
-        type="submit"
-        value="Iniciar sesión"
-      />
-    </form>
+    <div class="formulario">
+      <input type="text" v-model="username">
+      <input type="password" v-model="password">
+      <button @click="handleSubmit">Login</button>
+    </div>
+    
   </div>
 </template>
-<script script>
-</script>
-
 
 <style scoped>
-.login{
-    background-color: #dc6d3e;
+.login {
+  background-color: #dc6d3e;
 }
 </style>
