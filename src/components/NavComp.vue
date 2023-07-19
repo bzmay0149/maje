36fe569aa40f218c87b3945d1d64ed089e44aaf4
Shamanesss <mentulbenodorna@gmail.com>
2023-07-19 09:13:23 +0200

    d

--- src/components/NavComp.vue
+++ src/components/NavComp.vue
@@ -85,17 +85,12 @@
 
 
 @media (max-width: 680px) {
-<<<<<<< HEAD
   /* .nav-item {
     
     margin-left: 2.8rem;
   } */
   .nav-link {
     font-size: medium;
-=======
-   .nav-link {
-    font-size: x-large;
->>>>>>> MT
   }
 }
 </style>
