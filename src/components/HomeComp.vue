2c244d5579cc4edfde417d1a6fc21b367d0a5488
Shamanesss <mentulbenodorna@gmail.com>
2023-07-21 14:27:04 +0200

    language

--- src/components/HomeComp.vue
+++ src/components/HomeComp.vue
@@ -1,54 +1,27 @@
 <template>
-  <!-- <div class="row row-cols-1 row-cols-md-2 g-4 m-5">
-    <div class="col">
-      <div class="card border-0">
-        <img
-          src="../assets/surgery-room.jpg"
-          class="card-img-top rounded-4"
-          alt="..."
-        />
-      </div>
-    </div>
-    <div class="col d-flex align-items-center">
-      <div class="card title m-5 border-0">
-        <h5 class="card-title text-center">Título</h5>
-        <p class="card-text text-start text-md-center">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem
-          aspernatur officia hic in asperiores, ipsam labore dicta incidunt
-          nobis nemo soluta voluptates quos? Cumque, ut nihil dolor repellat
-          quaerat placeat?
-        </p>
-        <p class="card-text text-start text-md-center">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem
-          aspernatur officia hic in asperiores, ipsam labore dicta incidunt
-          nobis nemo soluta voluptates quos? Cumque, ut nihil dolor repellat
-          quaerat placeat?
-        </p>
-      </div>
-    </div>
-  </div> -->
-
+  <div>
+    <select v-model="$i18n.locale">
+      <option v-for="locale in $i18n.availableLocales" :key=" `locale-${locale}`" :value="locale">{{ locale }}</option>
+      
+    </select>
+  </div>
   <div class="row justify-content-evenly">
     <div class="col-md-6">
-      <img
-        class="img-fluid  mx-auto p-3 mt-5  d-block"
-        src="../assets/surgery-room.jpg"
-        alt=""
-      />
+      <img class="img-fluid  mx-auto p-3 mt-5  d-block" src="../assets/surgery-room.jpg" alt="" />
     </div>
     <div class="col-md-5">
-      <h1 class="text-center mx-auto ">Zurtek, S.L.</h1>
-      <p class="mobile-text">
-        Zurtek, S.L. es una empresa de inserción dentro del sector de la madera. Se ha creado en el año 2004 promovida por la cooperativa Peñascal, S.Coop. <br>
+      <h1 class="text-center mx-auto ">{{ $t('home.titulo') }}</h1>
+      <p class="mobile-text"> {{ $t('home.body') }}
+        <br>
 
-Entre sus objetivos está la inserción laboral de personas en riesgo de exclusión a través de la fabricación y montaje de todo tipo de mobiliario y elementos de carpintería, con el fin de que las personas en puestos de inserción adquieran las competencias necesarias para insertarse en el mercado laboral ordinario dentro de dicho sector.
-<br>
-<span>Servicios:</span>
-<br>
-Fabricación,montaje e instalación de mobiliario y elementos de carpintería. Diseño a medida de todo tipo de mobiliario  y soluciones constructivas. Desarrollo de productos dedicados a la agricultura urbana, vertical.
+        {{ $t('home.body1') }}
+        <br>
+        <span>{{ $t('home.subtitle') }}</span>
+        <br>
+        {{ $t('home.body2') }}
       </p>
-     
-      
+
+
     </div>
   </div>
 </template>
@@ -56,52 +29,59 @@ Fabricación,montaje e instalación de mobiliario y elementos de carpintería. D
 <script setup></script>
 
 <style scoped>
-
-
-.mobile-text{
+.mobile-text {
   text-align: justify;
 }
-span{
+
+span {
   font-weight: bold;
   font-size: x-large;
   padding-top: 5em;
 }
-.text-center{
+
+.text-center {
   margin-top: 2em;
   font-weight: bold;
 }
-.img-fluid{
+
+.img-fluid {
   margin-bottom: 3em;
   border-radius: 10%;
 }
+
 @media (max-width: 1200px) {
   .card-title {
-    font-size: medium; /* Tamaño de letra ajustado */
-    }
-    .text-center{
-  margin-top: 4em;
-  font-size: large; 
-  margin-bottom: .2em;
-  
-    }
-    span{
-      font-size: large;
-    }
-   
-    .align-items-center{
-      margin: 0%;
-      padding-left: auto;
-      padding-right: auto;
-    }
-    .mobile-text{
-      font-size: x-small;
-      
-    }
-  
-   
+    font-size: medium;
+    /* Tamaño de letra ajustado */
+  }
+
+  .text-center {
+    margin-top: 4em;
+    font-size: large;
+    margin-bottom: .2em;
+
+  }
+
+  span {
+    font-size: large;
+  }
+
+  .align-items-center {
+    margin: 0%;
+    padding-left: auto;
+    padding-right: auto;
+  }
+
+  .mobile-text {
+    font-size: x-small;
+
+  }
+
+
 
   .text-md-center {
-    text-align: center; /* Alineación centrada en tamaño de tablet */
+    text-align: center;
+    /* Alineación centrada en tamaño de tablet */
     font-size: small;
   }
 }
@@ -110,19 +90,21 @@ span{
   .mobile-text {
     text-align: justify;
     margin: 1rem;
-     padding: 5px; 
-    
+    padding: 5px;
+
 
   }
-  .text-center{
+
+  .text-center {
     margin-top: .5em;
   }
-  .img-fluid{
+
+  .img-fluid {
     margin-bottom: .2em;
-}
-.mobile-text2{
-  margin-bottom: 3em;
-  margin-top: 0%;
-}
-}
-</style>
+  }
+
+  .mobile-text2 {
+    margin-bottom: 3em;
+    margin-top: 0%;
+  }
+}</style>
---
+++ src/lang/messages.js
@@ -0,0 +1,71 @@
+export const messages = {
+    es:{
+        navbar:{
+            home: 'Inicio',
+            about: 'Servicios',
+            contact: 'Contacto',
+            admin: 'Administrador'
+        },
+        home: {
+            titulo: 'Zurtek, S.L.',
+            body: 'Zurtek, S.L. es una empresa de inserción dentro del sector de la madera. Se ha creado en el año 2004 promovida por la cooperativa Peñascal, S.Coop. ',
+            body1: 'Entre sus objetivos está la inserción laboral de personas en riesgo de exclusión a través de la fabricación y montaje de todo tipo de mobiliario y elementos de carpintería, con el fin de que las personas en puestos de inserción adquieran las competencias necesarias para insertarse en el mercado laboral ordinario dentro de dicho sector.',
+
+            subtitle: 'Servicios',
+            body2: 'Fabricación,montaje e instalación de mobiliario y elementos de carpintería. Diseño a medida de todo tipo de mobiliario  y soluciones constructivas. Desarrollo de productos dedicados a la agricultura urbana, vertical.'
+            },
+        about: {
+            title1: 'Hasiera',
+            title2: '   ',
+            title3:'  ',
+            title4:'  ',
+            title5:'  ',
+            title6:'  ',
+            title7:'',
+            title8:'    ',
+            title9:'    ',
+            title10:'    ', 
+
+            subtitle: 'Zerbitzuak'
+            },
+        contact: {
+            title: 'Hasiera',
+            subtitle: 'Zerbitzuak'
+            },
+        admin: {
+            title: 'Hasiera',
+            subtitle: 'Zerbitzuak'
+        }
+
+    },
+    eus:{
+        navbar:{
+            home: 'Hasiera',
+            about: 'Zerbitzuak',
+            contact: 'Kontaktua',
+            admin: 'kudeatzailea'
+
+        },
+
+        home: {
+            titulo: 'Zurtek, S.L.',
+            body: 'Zurtek, S.L., lan txertaketara bideraturik dagoen enpresa bat da, egur sektorean lan egiten duena. 2004. urtean sortu zan, Cooperativa Peñascal, S.Coop-en eskutik. ',
+            body1: 'Bere helburuen artean, bazterketa arriskuan dauden pertsonen lan-txertaketa dago, hau,  mota guztietako altzariak eta elementuak fabrikatuz eta muntatuz lortzen dute, zurgintza irakatsiz,  txertatze-postuetan dauden pertsonek sektore honetako lan-merkatuan sartzeko beharrezkoak diren gaitasunak eskura ditzaten',
+
+            subtitle: 'Zerbitzuak',
+            body2: 'Zurgintzako elementuen eta altzarien fabrikazioa, muntaketa eta instalazioa. Mota guztietako altzari diseinu pertsonalizatuak eta irtenbide berritzaileak. Eguneroko bizitzan baliagarriak izango diren produktuen garapena sustatuz.'
+            },
+        about: {
+            title: 'Hasiera',
+            subtitle: 'Zerbitzuak'
+            },
+        contact: {
+            title: 'Hasiera',
+            subtitle: 'Zerbitzuak'
+            },
+        admin: {
+            title: 'Hasiera',
+            subtitle: 'Zerbitzuak'
+        }
+    }
+}
