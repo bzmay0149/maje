1c353012c59abb5377bee5cbb2bfb8df3815a1e8
Shamanesss <118900582+Shamanesss@users.noreply.github.com>
2023-07-22 09:37:18 +0200

    idiomas  logo

--- src/components/NavComp.vue
+++ src/components/NavComp.vue
@@ -1,39 +1,52 @@
-
-
-
 <template>
   <div>
     <nav class="navbar navbar-light">
       <div class="navbar-content">
         <div class="navbar-logo">
           <div>
-          <router-link to="/" class="navbar-brand">
-            <img src="../assets/logo-zurtek.png" alt="logo-zurtek" width="160">
-          </router-link></div>
-          
-        
+            <router-link to="/" class="navbar-brand">
+              <img
+                src="../assets/logo-zurtek.png"
+                alt="logo-zurtek"
+                width="160"
+              />
+            </router-link>
+          </div>
+          <!-- utilizamos la libreria i18n  para cambiar el idioma del sitio web -->
           <div class="d-flex justify-content-end mx-3">
-  
-    <select v-model="$i18n.locale">
-      <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}</option>
-    </select>
- 
-</div></div>
+            <select v-model="$i18n.locale">
+              <option
+                v-for="locale in $i18n.availableLocales"
+                :key="`locale-${locale}`"
+                :value="locale"
+              >
+                {{ locale }}
+              </option>
+            </select>
+          </div>
+        </div>
         <div class="navbar-links">
           <ul class="navbar-nav d-flex flex-row">
             <li class="nav-item">
-              <router-link to="/" class="nav-link">{{ $t('navbar.home') }}</router-link>
+              <router-link to="/" class="nav-link">{{
+                $t("navbar.home")
+              }}</router-link>
             </li>
             <li class="nav-item">
-              <router-link to="/services" class="nav-link">{{ $t('navbar.about') }}</router-link>
+              <router-link to="/services" class="nav-link">{{
+                $t("navbar.about")
+              }}</router-link>
             </li>
             <li class="nav-item">
-              <router-link to="/about" class="nav-link">{{ $t('navbar.contact') }}</router-link>
+              <router-link to="/about" class="nav-link">{{
+                $t("navbar.contact")
+              }}</router-link>
             </li>
             <li class="nav-item">
-              <router-link to="/login" class="nav-link">{{ $t('navbar.admin') }}</router-link>
+              <router-link to="/login" class="nav-link">{{
+                $t("navbar.admin")
+              }}</router-link>
             </li>
-           
           </ul>
         </div>
       </div>
@@ -41,9 +54,7 @@
   </div>
 </template>
 
-<script setup>  
-
-</script>
+<script setup></script>
 
 <style scoped>
 .router-link-active {
@@ -52,8 +63,6 @@
   padding: 5px;
   margin: 5px;
   border: #f1ece6;
- 
-  
 }
 .navbar {
   background-color: transparent;
@@ -62,9 +71,7 @@
 
 .navbar-content {
   width: 100%;
- 
 }
-
 
 .navbar-logo {
   background-image: url(../assets/madera.png);
@@ -72,18 +79,17 @@
   background-size: cover;
   background-position: center;
   width: 100%;
-  padding-top:1rem;
+  padding-top: 1rem;
   padding-bottom: 1rem;
- 
 }
 
 .navbar-links {
-  background-color:rgb(75, 15, 15);
+  background-color: rgb(75, 15, 15);
   width: 100%;
 }
 
 .nav-link {
-  color: #D17F1F;
+  color: #d17f1f;
   font-size: xx-large;
   font-weight: bold;
 }
@@ -93,21 +99,16 @@
 }
 
 .nav-item {
- 
   margin-left: auto;
   margin-right: auto;
 }
-select{
+select {
   background-color: rgb(75, 15, 15);
   color: #f1ece6;
 }
 
-
-
-
-
 @media (max-width: 680px) {
-.nav-link {
+  .nav-link {
     font-size: medium;
   }
 }
